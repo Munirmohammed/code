ded48d1233bc9af470de4c2b1ebe9acb6a204e06
Munir Mohammed <munirmohammed038@gmail.com>
2023-05-10 19:02:57 +0300

    Time: 184 ms (6.38%) | Memory: 50.1 MB (5.95%) - LeetSync

--- array-reduce-transformation/array-reduce-transformation.js
+++ array-reduce-transformation/array-reduce-transformation.js
@@ -1,9 +1,3 @@
-/**
- * @param {number[]} nums
- * @param {Function} fn
- * @param {number} init
- * @return {number}
- */
 var reduce = function(nums, fn, init) {
     ans = init
     for(const i of nums){

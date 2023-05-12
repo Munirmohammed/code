4f6df8f9b6058a4ebf8438c77b1cbb73b1ff1624
Munir Mohammed <munirmohammed038@gmail.com>
2023-05-12 07:57:04 +0300

    Time: 47 ms (20.68%) | Memory: 16.2 MB (15.76%) - LeetSync

---
+++ integer-break/integer-break.py
@@ -0,0 +1,10 @@
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        if n <= 3:
+            return n - 1
+        elif n%3 == 0:
+            return 3**(int(n/3))
+        elif n%3 == 1:
+            return 3**(int(n/3)-1)*4
+        else:
+            return 3**(int(n/3))*2

dfb20ab19e36f1284e0952b452c7b4ca910dc4d7
Munir Mohammed <munirmohammed038@gmail.com>
2023-05-16 20:40:03 +0300

    Time: 749 ms (92.67%) | Memory: 22.4 MB (23.25%) - LeetSync

---
+++ k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -0,0 +1,4 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        points = sorted(points,key = lambda x: pow (x[0]*x[0] +  x[1]*x[1] ,0.5))
+        return points[:k]

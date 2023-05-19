fba2d7176149a5880e40567706040b0f69c941b7
Mohammed Munir <munirmohammed038@gmail.com>
2023-05-19 15:07:48 +0300

    Time: 47 ms (17.04%) | Memory: 16.2 MB (21.64%) - LeetSync

---
+++ binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
@@ -0,0 +1,3 @@
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        return self.postorderTraversal(root.left) + self.postorderTraversal(root.right) + [root.val]  if root else []

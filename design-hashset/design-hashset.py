d9d3dfd7409d3d7fb772da028a889ac0a27d29af
Mohammed Munir <munirmohammed038@gmail.com>
2023-05-30 07:23:17 +0300

    Time: 2564 ms (5.50%) | Memory: 21.4 MB (56.20%) - LeetSync

---
+++ design-hashset/design-hashset.py
@@ -0,0 +1,20 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.x = []
+
+    def add(self, key: int) -> None:
+        if not self.contains(key):
+            self.x.append(key)
+
+    def remove(self, key: int) -> None:
+        for i in range(len(self.x)):
+            if self.x[i] == key:
+                self.x.pop(i)
+                break
+
+    def contains(self, key: int) -> bool:
+        if key in self.x:
+            return True
+        else:
+            return False

525ba443c3fefa8f8eadd9cb3a127369c6900662
Mohammed Munir <munirmohammed038@gmail.com>
2023-07-08 15:10:52 +0300

    Time: 701 ms (96.46%) | Memory: 29.9 MB (55.75%) - LeetSync

---
+++ put-marbles-in-bags/put-marbles-in-bags.py
@@ -0,0 +1,5 @@
+class Solution:
+    def putMarbles(self, weights: List[int], k: int) -> int:
+        arr = sorted(map(sum,(pairwise(weights))))
+        return sum(arr[-k+1:]) - sum(arr[:k-1]) if k > 1 else 0
+        

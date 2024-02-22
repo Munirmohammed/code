d015a7033735bcfffd5e6d987456291934df755a
Mohammed Munir <munirmohammed038@gmail.com>
2024-02-22 11:29:22 +0300

    Time: 593 ms (85.10%) | Memory: 21.7 MB (66.79%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.py
+++ 1039-find-the-town-judge/find-the-town-judge.py
@@ -1,12 +1,10 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        if not trust and n == 1:
-            return 1
-        degree = [0 for i in range(0,n+1)]
-        for i,j in trust:
-            degree[i] -= 1
-            degree[j] += 1
-        for i in degree:
-            if i == n-1:
-                return degree.index(i)
-        return -1
+        trust_count = [0] * (n + 1)
+        for a, b in trust:
+            trust_count[a] -= 1  
+            trust_count[b] += 1 
+        for i in range(1, n + 1):
+            if trust_count[i] == n - 1:
+                return i
+        return -1 

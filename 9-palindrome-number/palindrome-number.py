aa0cfdb64ce0056b5cceb49ce8883d2aefc4a2f3
Mohammed Munir <munirmohammed038@gmail.com>
2024-03-15 11:05:19 +0300

    Time: 38 ms (67.61%) | Memory: 11.7 MB (18.17%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -2,5 +2,9 @@ class Solution(object):
     def isPalindrome(self, x):
         if x < 0:
             return False
-        else:
-            return str(x) == str(x)[::-1]
+        reversed_x = 0
+        original_x = x
+        while x > 0:
+            reversed_x = reversed_x * 10 + x % 10
+            x = x // 10
+        return original_x == reversed_x

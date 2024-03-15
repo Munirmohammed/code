b7c8b1a11fe168a3796ec0e9286c3714e80bc937
Mohammed Munir <munirmohammed038@gmail.com>
2024-03-15 10:33:08 +0300

    Time: 2 ms (73.94%) | Memory: 53 MB (77.29%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -4,11 +4,11 @@ class Solution {
         int[] output = new int[n];
         output[0] = 1;
         for (int i = 1; i < n; i++) {
-            output[i] = nums[i-1] * output[i-1];
+            output[i] = nums[i - 1] * output[i - 1];
         }
 
         int right = 1;
-        for (int i = n-1; i >= 0; i--) {
+        for (int i = n - 1; i >= 0; i--) {
             output[i] *= right;
             right *= nums[i];
         }

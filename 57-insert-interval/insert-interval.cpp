e4752ef63cffe117c73a0a9b7df4a3d7832465c7
Mohammed Munir <munirmohammed038@gmail.com>
2024-03-18 14:34:52 +0300

    Time: 13 ms (48.78%) | Memory: 20.4 MB (84.54%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -3,26 +3,19 @@ public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
        vector<vector<int>> result; // Corrected line
         
-        bool inserted = false;
-        
         for (auto& interval : intervals) {
             if (interval[1] < newInterval[0]) {
                 result.push_back(interval);
             } else if (interval[0] > newInterval[1]) {
-                if (!inserted) {
-                    result.push_back(newInterval);
-                    inserted = true;
-                }
-                result.push_back(interval);
+                result.push_back(newInterval);
+                newInterval = interval;
             } else {
                 newInterval[0] = min(interval[0], newInterval[0]);
                 newInterval[1] = max(interval[1], newInterval[1]);
             }
         }
         
-        if (!inserted) {
-            result.push_back(newInterval);
-        }
+        result.push_back(newInterval);
         
         return result;
     }

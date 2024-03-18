761879e9e12fcfd884c51b79e9e6686b3dba4559
Mohammed Munir <munirmohammed038@gmail.com>
2024-03-18 11:10:05 +0300

    Time: 212 ms (61.74%) | Memory: 72.6 MB (74.86%) - LeetSync

---
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.js
@@ -0,0 +1,20 @@
+var findMinArrowShots = function(points) {
+    if (points.length === 0) {
+        return 0;
+    }
+
+    // Sort the balloons by their right endpoints
+    points.sort((a, b) => a[1] - b[1]);
+
+    let end = points[0][1];
+    let count = 1;
+
+    for (let i = 1; i < points.length; i++) {
+        if (points[i][0] > end) {
+            count++;
+            end = points[i][1];
+        }
+    }
+
+    return count;
+    }

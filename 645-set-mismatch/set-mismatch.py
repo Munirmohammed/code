77e87d0a240cbc065e9e6127faa44af2867b987a
Mohammed Munir <munirmohammed038@gmail.com>
2024-03-19 19:13:37 +0300

    Time: 147 ms (89.75%) | Memory: 17.7 MB (95.95%) - LeetSync

--- 645-set-mismatch/set-mismatch.py
+++ 645-set-mismatch/set-mismatch.py
@@ -1,20 +1,20 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        # Calculate the sum of all numbers and the sum of squares
-        actual_sum = sum(nums)
+        actual_sum = 0
+        actual_sum_of_squares = 0
         expected_sum = n * (n + 1) // 2
-        actual_sum_of_squares = sum(num * num for num in nums)
         expected_sum_of_squares = sum(num * num for num in range(1, n + 1))
         
-        # Calculate the difference between actual and expected sums
+        for num in nums:
+            actual_sum += num
+            actual_sum_of_squares += num * num
+        
         sum_diff = actual_sum - expected_sum
         sum_of_squares_diff = actual_sum_of_squares - expected_sum_of_squares
         
-        # Calculate the duplicate number
         duplicate = (sum_of_squares_diff // sum_diff + sum_diff) // 2
         
-        # Calculate the missing number
         missing = duplicate - sum_diff
         
         return [duplicate, missing]

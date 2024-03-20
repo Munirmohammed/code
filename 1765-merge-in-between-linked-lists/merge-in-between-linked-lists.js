8b8d43fce8f5d2fc5bb18d7ebc1380bdbb34b063
Mohammed Munir <munirmohammed038@gmail.com>
2024-03-20 13:23:03 +0300

    Time: 146 ms (84.15%) | Memory: 62.1 MB (51.22%) - LeetSync

---
+++ 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.js
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {number} a
+ * @param {number} b
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeInBetween = function(list1, a, b, list2) {
+    // Find the node before the removal range
+    let prev = null;
+    let curr = list1;
+    for (let i = 0; i < a; i++) {
+        prev = curr;
+        curr = curr.next;
+    }
+
+    // Remove the nodes in the range [a, b]
+    let nextNode = curr.next;
+    for (let i = 0; i < b - a; i++) {
+        curr.next = nextNode;
+        curr = nextNode;
+        if (nextNode) {
+            nextNode = nextNode.next;
+        }
+    }
+
+    // Insert list2 into the removed range
+    if (prev) {
+        prev.next = list2;
+    } else {
+        list1 = list2;
+    }
+    curr = list2;
+    while (curr.next) {
+        curr = curr.next;
+    }
+    curr.next = nextNode;
+
+    return list1;
+};

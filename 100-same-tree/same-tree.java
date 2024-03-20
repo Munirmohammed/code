340a7884132777556ff3ff02dfb7978c3fbb6f8f
Mohammed Munir <munirmohammed038@gmail.com>
2024-03-20 16:06:30 +0300

    Time: 0 ms (100.00%) | Memory: 40.9 MB (46.46%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -6,9 +6,46 @@ class Solution {
         if (p == null || q == null) {
             return false;
         }
-        if (p.val != q.val) {
-            return false;
+        
+        LinkedList queueP = new LinkedList<>();
+        LinkedList queueQ = new LinkedList<>();
+        
+        queueP.add(p);
+        queueQ.add(q);
+        
+        while (!queueP.isEmpty() && !queueQ.isEmpty()) {
+            TreeNode nodeP = (TreeNode) queueP.poll();
+            TreeNode nodeQ = (TreeNode) queueQ.poll();
+            
+            if (nodeP.val != nodeQ.val) {
+                return false;
+            }
+            
+            if ((nodeP.left == null && nodeQ.left != null) || (nodeP.left != null && nodeQ.left == null)) {
+                return false;
+            }
+            
+            if ((nodeP.right == null && nodeQ.right != null) || (nodeP.right != null && nodeQ.right == null)) {
+                return false;
+            }
+            
+            if (nodeP.left != null) {
+                queueP.add(nodeP.left);
+            }
+            
+            if (nodeP.right != null) {
+                queueP.add(nodeP.right);
+            }
+            
+            if (nodeQ.left != null) {
+                queueQ.add(nodeQ.left);
+            }
+            
+            if (nodeQ.right != null) {
+                queueQ.add(nodeQ.right);
+            }
         }
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        
+        return queueP.isEmpty() && queueQ.isEmpty();
     }
 }

fc0fe53f104b31f5e144072b0fbbf1ecc969ad5d
Mohammed Munir <munirmohammed038@gmail.com>
2024-03-20 17:29:40 +0300

    Time: 87 ms (54.81%) | Memory: 56.9 MB (84.83%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.js
+++ 1039-find-the-town-judge/find-the-town-judge.js
@@ -1,11 +1,11 @@
 var findJudge = function(n, trust) {
-     var trust_counts = new Array(n + 1).fill(0);
+     var trustCounts = new Array(n + 1).fill(0);
         for(var i = 0; i < trust.length; i++) {
-            trust_counts[trust[i][0]]--;
-            trust_counts[trust[i][1]]++;
+            trustCounts[trust[i][0]]--;
+            trustCounts[trust[i][1]]++;
         }
         for(var i = 1; i <= n; i++) {
-            if(trust_counts[i] === n - 1) {
+            if(trustCounts[i] === n - 1) {
                 return i;
             }
         }

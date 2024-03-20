4776cc7bad21eb4cc15deffe6e0f3b051969cad4
Mohammed Munir <munirmohammed038@gmail.com>
2024-03-20 15:23:59 +0300

    Time: 38 ms (36.29%) | Memory: 16.7 MB (10.24%) - LeetSync

---
+++ 100-same-tree/same-tree.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p is None and q is None:
+            return True
+        if p is None or q is None:
+            return False
+        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)

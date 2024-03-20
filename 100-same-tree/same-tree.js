7ab1b0d05b5d22532bf9628eb337718caccf0847
Mohammed Munir <munirmohammed038@gmail.com>
2024-03-20 15:38:40 +0300

    Time: 71 ms (6.28%) | Memory: 49.1 MB (47.20%) - LeetSync

--- 100-same-tree/same-tree.js
+++ 100-same-tree/same-tree.js
@@ -5,5 +5,42 @@ var isSameTree = function(p, q) {
     if (p === null || q === null) {
         return false;
     }
-    return p.val === q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    
+    let queueP = [p];
+    let queueQ = [q];
+    
+    while (queueP.length > 0 && queueQ.length > 0) {
+        let nodeP = queueP.shift();
+        let nodeQ = queueQ.shift();
+        
+        if (nodeP.val !== nodeQ.val) {
+            return false;
+        }
+        
+        if ((nodeP.left === null && nodeQ.left !== null) || (nodeP.left !== null && nodeQ.left === null)) {
+            return false;
+        }
+        
+        if ((nodeP.right === null && nodeQ.right !== null) || (nodeP.right !== null && nodeQ.right === null)) {
+            return false;
+        }
+        
+        if (nodeP.left !== null) {
+            queueP.push(nodeP.left);
+        }
+        
+        if (nodeP.right !== null) {
+            queueP.push(nodeP.right);
+        }
+        
+        if (nodeQ.left !== null) {
+            queueQ.push(nodeQ.left);
+        }
+        
+        if (nodeQ.right !== null) {
+            queueQ.push(nodeQ.right);
+        }
+    }
+    
+    return queueP.length === 0 && queueQ.length === 0;
 };

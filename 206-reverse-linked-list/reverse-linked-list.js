44aefe2d949e1a386b0446141388f25c95091522
Mohammed Munir <munirmohammed038@gmail.com>
2024-03-21 11:37:28 +0300

    Time: 68 ms (12.03%) | Memory: 52.4 MB (10.33%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.js
+++ 206-reverse-linked-list/reverse-linked-list.js
@@ -1,11 +1,30 @@
 var reverseList = function(head) {
-    if (head === null || head.next === null) {
-        return head;
+    // Create an array to store the values of the linked list
+    let array = [];
+    
+    // Traverse the linked list and add the values to the array
+    let current = head;
+    while (current !== null) {
+        array.push(current.val);
+        current = current.next;
     }
-
-    let newHead = reverseList(head.next);
-    head.next.next = head;
-    head.next = null;
-
-    return newHead;
+    
+    // Reverse the array
+    array.reverse();
+    
+    // Create a new linked list using the reversed array
+    let reversedHead = null;
+    let reversedCurrent = null;
+    for (let i = 0; i < array.length; i++) {
+        let newNode = new ListNode(array[i]);
+        if (reversedHead === null) {
+            reversedHead = newNode;
+            reversedCurrent = newNode;
+        } else {
+            reversedCurrent.next = newNode;
+            reversedCurrent = newNode;
+        }
+    }
+    
+    return reversedHead;
 };

67ead2980f450f9b5755527135524a1c3900411f
Mohammed Munir <munirmohammed038@gmail.com>
2024-03-21 13:06:04 +0300

    Time: 9 ms (29.87%) | Memory: 6.1 MB (47.91%) - LeetSync

---
+++ 31-next-permutation/next-permutation.c
@@ -0,0 +1,24 @@
+void nextPermutation(int* nums, int numsSize) {
+    int i = numsSize - 2;
+    while (i >= 0 && nums[i] >= nums[i + 1]) {
+        i--;
+    }
+    if (i >= 0) {
+        int j = numsSize - 1;
+        while (j > i && nums[j] <= nums[i]) {
+            j--;
+        }
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    int left = i + 1;
+    int right = numsSize - 1;
+    while (left < right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+        left++;
+        right--;
+}
+}

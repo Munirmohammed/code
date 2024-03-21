284203eae6dafa15e6d4934ab9f5351f71410f47
Mohammed Munir <munirmohammed038@gmail.com>
2024-03-21 14:17:48 +0300

    Time: 41 ms (38.66%) | Memory: 16.5 MB (73.97%) - LeetSync

---
+++ 60-permutation-sequence/permutation-sequence.py
@@ -0,0 +1,19 @@
+class Solution:
+    def getPermutation(self, n: int, k: int) -> str:
+        def factorial(n):
+            result = 1
+            while n > 0:
+                result *= n
+                n -= 1
+            return result
+        
+        nums = list(range(1, n+1))
+        k -= 1
+        result = []
+        while n > 0:
+            factorial_n_minus_1 = factorial(n-1)
+            index, k = divmod(k, factorial_n_minus_1)
+            result.append(nums[index])
+            nums.pop(index)
+            n -= 1
+        return ''.join(map(str, result))

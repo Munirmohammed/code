f2250e3fbe1e71877804ec4b95fef15d328c9daf
Mohammed Munir <munirmohammed038@gmail.com>
2024-03-21 12:33:45 +0300

    Time: 45 ms (44.88%) | Memory: 16.6 MB (52.89%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -1,13 +1,18 @@
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
-        n = len(nums)
-        i = n - 2
-        while i >= 0 and nums[i] >= nums[i + 1]:
+        i = len(nums) - 1
+        while i > 0 and nums[i - 1] >= nums[i]:
             i -= 1
-        if i >= 0:
-            j = n - 1
-            while j > i and nums[j] <= nums[i]:
-                j -= 1
-            nums[i], nums[j] = nums[j], nums[i]
-        nums[i + 1:] = nums[i + 1:][::-1]
+        if i == 0:
+            nums.sort()
+            return
+        j = len(nums) - 1
+        while nums[j] <= nums[i - 1]:
+            j -= 1
+        nums[i - 1], nums[j] = nums[j], nums[i - 1]
+        left, right = i, len(nums) - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
         

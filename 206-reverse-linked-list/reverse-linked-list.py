ba2b695537b3a938093fc9e77430b9dfe0f4de80
Mohammed Munir <munirmohammed038@gmail.com>
2024-03-22 15:34:25 +0300

    Time: 39 ms (46.24%) | Memory: 17.9 MB (5.56%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -4,11 +4,28 @@ class Solution:
             return head
 
 
-        curr, prev = head, None
+        class Node:
+            def __init__(self, val):
+                self.val = val
+                self.next = None
+
+        curr = head
+        prev = None
+
         while curr:
             next_node = curr.next
             curr.next = prev
             prev = curr
             curr = next_node
-        
-        return prev
+
+        # Create a new node and assign the reversed list to it
+        new_head = Node(None)
+        reversed_list = prev
+
+        curr = new_head
+        while reversed_list:
+            curr.next = Node(reversed_list.val)
+            reversed_list = reversed_list.next
+            curr = curr.next
+
+        return new_head.next

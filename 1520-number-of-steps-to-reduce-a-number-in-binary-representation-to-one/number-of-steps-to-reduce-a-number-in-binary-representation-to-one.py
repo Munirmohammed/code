c3ae1494efe617616dde16e68d35f535bca7d4ae
Mohammed Munir <munirmohammed038@gmail.com>
2024-05-30 08:38:23 +0300

    Time: 34 ms (75.78%) | Memory: 16.5 MB (61.96%) - LeetSync

--- 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
@@ -3,10 +3,10 @@ class Solution:
         num = int(s, 2)  #Convert binary string to integer
         steps = 0
 
-        while num > 1:  #Loop until num equals 1
-            if num % 2 == 0: 
-                num //= 2 # If even, divide by 2
-            else: 
-                num += 1 # If odd, add 1
-            steps += 1 # Increment steps count
+        while num != 1:
+            if num % 2 == 0:
+                num //= 2
+            else:
+                num += 1
+            steps += 1
         return steps

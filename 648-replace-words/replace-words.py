1d17108168f214efba2bb34ce8f3bbc03725113e
Mohammed Munir <munirmohammed038@gmail.com>
2024-06-14 15:33:51 +0300

    Time: 96 ms (85.05%) | Memory: 32.8 MB (17.46%) - LeetSync

--- 648-replace-words/replace-words.py
+++ 648-replace-words/replace-words.py
@@ -1,31 +1,35 @@
-class Solution:
-    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
-        # Build Trie from dictionary
-        root = TrieNode()
-        for word in dictionary:
-            node = root
-            for ch in word:
-                if ch not in node.children:
-                    node.children[ch] = TrieNode()
-                node = node.children[ch]
-            node.endOfWord = True
-
-        # Replace words in sentence
-        words = sentence.split()
-        for i in range(len(words)):
-            node = root
-            for j in range(len(words[i])):
-                if words[i][j] in node.children:
-                    node = node.children[words[i][j]]
-                    if node.endOfWord:
-                        words[i] = words[i][:j+1]
-                        break
-                else:
-                    break
-
-        return ' '.join(words)
-
 class TrieNode:
     def __init__(self):
         self.children = {}
-        self.endOfWord = False
+        self.isEnd = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for ch in word:
+            if ch not in node.children:
+                node.children[ch] = TrieNode()
+            node = node.children[ch]
+        node.isEnd = True
+
+class Solution:
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        trie = Trie()
+        for word in dictionary:
+            trie.insert(word)
+
+        words = sentence.split()
+        for i in range(len(words)):
+            node = trie.root
+            for j in range(len(words[i])):
+                if words[i][j] not in node.children:
+                    break
+                node = node.children[words[i][j]]
+                if node.isEnd:
+                    words[i] = words[i][:j+1]
+                    break
+
+        return ' '.join(words)

7cf4447257c336e8af0906795811caa5190ec05b
Mohammed Munir <munirmohammed038@gmail.com>
2024-06-15 08:02:04 +0300

    Time: 282 ms (57.72%) | Memory: 85.7 MB (32.52%) - LeetSync

---
+++ 502-ipo/ipo.js
@@ -0,0 +1,79 @@
+class MaxBinaryHeap {
+  constructor() {
+    this.values = [];
+  }
+  insert(value) {
+    this.values.push(value);
+    this.bubbleUp();
+  }
+  extractMax() {
+    const max = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.bubbleDown();
+    }
+    return max;
+  }
+  bubbleUp() {
+    let index = this.values.length - 1;
+    const element = this.values[index];
+    while (index > 0) {
+      let parentIndex = Math.floor((index - 1) / 2);
+      let parent = this.values[parentIndex];
+      if (element <= parent) break;
+      this.values[parentIndex] = element;
+      this.values[index] = parent;
+      index = parentIndex;
+    }
+  }
+  bubbleDown() {
+    let index = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIndex = 2 * index + 1;
+      let rightChildIndex = 2 * index + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIndex < length) {
+        leftChild = this.values[leftChildIndex];
+        if (leftChild > element) {
+          swap = leftChildIndex;
+        }
+      }
+      if (rightChildIndex < length) {
+        rightChild = this.values[rightChildIndex];
+        if (
+          (swap === null && rightChild > element) ||
+          (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rightChildIndex;
+        }
+      }
+
+      if (swap === null) break;
+      this.values[index] = this.values[swap];
+      this.values[swap] = element;
+      index = swap;
+    }
+  }
+}
+
+var findMaximizedCapital = function(k, w, profits, capital) {
+  const projects = profits.map((profit, i) => [capital[i], profit]);
+  projects.sort((a, b) => a[0] - b[0]);
+  const pq = new MaxBinaryHeap();
+  let i = 0;
+  while (k > 0) {
+    while (i < projects.length && projects[i][0] <= w) {
+      pq.insert(projects[i][1]);
+      i++;
+    }
+    if (pq.values.length === 0) break;
+    w += pq.extractMax();
+    k--;
+  }
+  return w;
+};

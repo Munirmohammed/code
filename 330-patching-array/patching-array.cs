f30d2ceda8e080b47c579b862af09d3ab6b8653d
Mohammed Munir <munirmohammed038@gmail.com>
2024-06-16 06:56:42 +0300

    Time: 73 ms (75.00%) | Memory: 41.8 MB (100.00%) - LeetSync

--- 330-patching-array/patching-array.cs
+++ 330-patching-array/patching-array.cs
@@ -1,19 +1,19 @@
 public class Solution {
     public int MinPatches(int[] nums, int n) {
-        long miss = 1;  // Track the smallest missing sum
-        int patches = 0, i = 0; // Count of patches and index in nums
+        long miss = 1;  // The smallest sum we haven't achieved yet
+        int patches = 0, i = 0;
 
-        while (miss <= n) 
+        while (miss <= n)
         {
             if (i < nums.Length && nums[i] <= miss)
             {
-                // If the current num can be used to form sums, update miss
-                miss += nums[i++];
+                // If the current num can extend our reach, use it
+                miss += nums[i++]; 
             }
             else
             {
-                // Need a patch to cover the missing sum
-                miss += miss; 
+                // Otherwise, patch with the 'miss' value
+                miss += miss;
                 patches++;
             }
         }

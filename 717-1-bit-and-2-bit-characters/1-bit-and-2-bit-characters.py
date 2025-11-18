3534e203c96bb4ba8718448ebe195a96b2d597bc
Munir Mohammed <munirmohammed038@gmail.com>
2025-11-18 11:23:27 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (40.84%) - LeetSync

---
+++ 717-1-bit-and-2-bit-characters/1-bit-and-2-bit-characters.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isOneBitCharacter(self, bits: List[int]) -> bool:
+        i = 0
+        n = len(bits)
+        while i < n - 1:
+            if bits[i] == 1:
+                i += 2
+            else:
+                i += 1
+        return i == n - 1
